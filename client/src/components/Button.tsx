e2da1adb6358924d2fb4678fd505edd0b2bc6e08
Shubham <shubham.shubham.singh13@gmail.com>
2024-10-10 17:31:26 +0530

    feat: update HomePage, Button, and InputField components to pass UI test cases

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,5 +1,16 @@
-const Button = () => {
-  return <button>Click Me</button>;
+import React from "react";
+
+interface ButtonProps {
+  onClick: () => void;
+  children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
+  return (
+    <button onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -1,6 +1,24 @@
-const InputField = () => {
-    return <button>Click Me</button>;
-  };
-  
-  export default InputField;
-  
+import React from "react";
+
+interface InputFieldProps {
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const InputField: React.FC<InputFieldProps> = ({
+  placeholder,
+  value,
+  onChange,
+}) => {
+  return (
+    <input
+      type="text"
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  );
+};
+
+export default InputField;
